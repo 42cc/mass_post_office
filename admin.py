5dd3951f8e1d658ab0257e5e68b3f6c5a1a6bd78
Yaroslav Klyuyev <imposeren@gmail.com>
2013-11-15 13:54:44 +0200

    using OneToOne field

--- admin.py
+++ admin.py
@@ -1,10 +1,11 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 
 from .models import MailingList
 
 
 class MailingListAdmin(admin.ModelAdmin):
-    list_display = ('name', 'user_should_be_agree',)
+    list_display = ('name', 'user_should_be_agree', 'all_users')
     readonly_fields = ('or_list',)
     search_fields = ('name',)
 
--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
 from django.contrib.auth.models import User
@@ -7,7 +8,7 @@ from django.utils import simplejson
 
 
 class SubscriptionSettings(models.Model):
-    user = models.ForeignKey(User)
+    user = models.OneToOneField(User)
     subscribed = models.BooleanField(default=True)
 
     def __unicode__(self):
@@ -17,12 +18,12 @@ class SubscriptionSettings(models.Model):
 class MailingList(models.Model):
     name = models.CharField(verbose_name='List name', max_length=255)
     user_should_be_agree = models.BooleanField(
-            verbose_name='User should be agree', default=True)
+        verbose_name='User should be agree', default=True)
     all_users = models.BooleanField(
-            verbose_name='All users', default=False)
+        verbose_name='All users', default=False)
     or_list = models.TextField(verbose_name='json OR-list', default='')
     additional_users = models.ManyToManyField(
-            User, verbose_name='Additional users', null=True)
+        User, verbose_name='Additional users', null=True)
 
     def __unicode__(self):
         return self.name
@@ -44,12 +45,12 @@ class MailingList(models.Model):
     def get_users_queryset(self):
         if self.all_users:
             return User.objects.filter(
-                    is_active=True, email__isnull=False).exclude(email='')
+                is_active=True, email__isnull=False).exclude(email='')
         q = Q()
         if self.additional_users.exists():
             q |= Q(id__in=self.additional_users.values_list('id', flat=True))
         if self.or_list:
-             for i in simplejson.loads(self.or_list):
+            for i in simplejson.loads(self.or_list):
                 q |= Q(**i)
         q &= Q(is_active=True, email__isnull=False)
         return User.objects.filter(q).exclude(email='')
